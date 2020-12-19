478063b4caf52d9ba8f621837edf7c0940bd6b2e
Rozan Mhrzn <36452547+Rojanmhj2055@users.noreply.github.com>
2020-12-19 01:27:59 -0800

    added textfield view and starting on midview

--- lib/weather_forcast.dart
+++ lib/weather_forcast.dart
@@ -18,10 +18,54 @@ class _WeatherAppState extends State<WeatherApp>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title:Text("Weather App"),
-        
-      ),
-    );
-  }
+     body: ListView(
+       children: <Widget>[
+         textFieldView(),
+         Container(
+           //Future Builder is expectin WeatherForecast Model
+           //isn't necessary to write
+           child:FutureBuilder<WeatherForecastModel>(
+             future: forcastObject,
+             builder: (BuildContext context, AsyncSnapshot<WeatherForecastModel> snapshot){
+               if(snapshot.hasData)
+               {
+                  return Column(
+                    children: <Widget>[
+                      mid_view(snapshot),
+                    ]
+                  );
+               }else{
+                 return Container(
+                   child: Center(
+                     child:CircularProgressIndicator()),
+                   );
+               }
+              
+             },
+             ),
+         )
+                ],
+              ),
+         
+             );
+           }
+         
+           Widget textFieldView() {
+             return Container(
+               child: TextField(
+                 decoration:InputDecoration(
+                   hintText: "Enter City Name",
+                   prefixIcon: Icon(Icons.search),
+                   border: OutlineInputBorder(
+                     borderRadius: BorderRadius.circular(10.0),),
+                  contentPadding: EdgeInsets.all(8.5),
+                 ),
+                 onSubmitted:(value)=>{
+                    
+                 },
+               ),
+             );
+           }
+
+ 
 }
